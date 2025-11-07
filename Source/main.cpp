ae9cbaf12818a4cd4e7d4fa2b4ecc3c5473b31b5
Aiden Sanders <aidensandersbiz@gmail.com>
2025-11-06 21:18:11 -0700

    Move to smart pointers

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,28 +1,29 @@
 #include "ImageRec/onnx_classifier.h"
 #include "Camera/GStreamer.h"
 
+#include <memory>
+
 int main()
 {
     int W = 640, H = 480, FPS = 30;
-    cv::VideoCapture cap;
 
     // Initialize ONNX Classifier
     std::string model = "build/Assets/onnx/mobilenetv2-12-int8.onnx";
     std::string labels = "build/Assets/onnx/imagenet-simple-labels.json";
     int threads = 2;
 
-    ONNXClassifier clf;
-    if (!clf.Initialize(model, labels, threads))
+    std::unique_ptr<ONNXClassifier> clf = std::make_unique<ONNXClassifier>();
+    if (!clf->Initialize(model, labels, threads))
     {
         std::cerr << "ONNXClassifier failed to initialize. Check model path and files. Exiting.\n";
         return 1;
     }
 
-    GStreamer gst;
-    if (!gst.openCapture(GStreamer::CaptureBackend::LIBCAMERA, W, H, FPS))
+    std::unique_ptr<GStreamer> gst = std::make_unique<GStreamer>();
+    if (!gst->openCapture(GStreamer::CaptureBackend::LIBCAMERA, W, H, FPS))
     {
         std::cerr << "Failed to open GStreamer capture with LIBCAMERA backend. Trying V4L2...\n";
-        if (!gst.openCapture(GStreamer::CaptureBackend::V4L2, W, H, FPS))
+        if (!gst->openCapture(GStreamer::CaptureBackend::V4L2, W, H, FPS))
         {
             std::cerr << "Failed to open GStreamer capture with V4L2 backend. Exiting.\n";
             return 1;
@@ -35,12 +36,12 @@ int main()
 
     while (true)
     {
-        cv::Mat frame = gst.captureFrame();
+        cv::Mat frame = gst->captureFrame();
 
         currentTime = static_cast<float>(cv::getTickCount()) / cv::getTickFrequency() * 1000.0f;
         if (!(currentTime - lastPredictionTime < predictionDelay))
         {
-            auto res = clf.classify(frame, 1);
+            auto res = clf->classify(frame, 1);
             std::cout << "Top prediction:\n";
             if (!res.empty())
             {
